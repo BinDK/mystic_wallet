268b125dc1e36c1aa6f9efa8f233728d27ca54cb
BinDK <sarakenny123@yahoo.com>
2025-09-27 10:21:24 +0700

    chore: downcase category name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
 expense_categories = [
-  { name: "Food", category_type: "expense" },
-  { name: "Coffee", category_type: "expense" },
-  { name: "Rent", category_type: "expense" },
-  { name: "Gas", category_type: "expense" },
-  { name: "Groceries", category_type: "expense" },
-  { name: "Entertainment", category_type: "expense" },
-  { name: "Utilities", category_type: "expense" },
-  { name: "Insurance", category_type: "expense" },
-  { name: "Medical", category_type: "expense" },
-  { name: "Shopping", category_type: "expense" },
-  { name: "Transportation", category_type: "expense" }
+  { name: "food", category_type: "expense" },
+  { name: "coffee", category_type: "expense" },
+  { name: "rent", category_type: "expense" },
+  { name: "gas", category_type: "expense" },
+  { name: "groceries", category_type: "expense" },
+  { name: "entertainment", category_type: "expense" },
+  { name: "utilities", category_type: "expense" },
+  { name: "insurance", category_type: "expense" },
+  { name: "medical", category_type: "expense" },
+  { name: "shopping", category_type: "expense" },
+  { name: "transportation", category_type: "expense" }
 ]
 
 income_categories = [
-  { name: "Salary", category_type: "income" },
-  { name: "Investment Return", category_type: "income" },
-  { name: "Freelance", category_type: "income" },
-  { name: "Bonus", category_type: "income" }
+  { name: "salary", category_type: "income" },
+  { name: "investment Return", category_type: "income" },
+  { name: "freelance", category_type: "income" },
+  { name: "bonus", category_type: "income" }
 ]
 
 expense_categories.each do |category_attrs|
